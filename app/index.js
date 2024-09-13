4b56d97c36a8d5d726213a0f5cf536ecf9ba3343
Caroline-A-Martins <business.carolmartins@gmail.com>
2024-09-13 16:22:33 -0300

    Aula 1

---
+++ app/index.js
@@ -0,0 +1,14 @@
+// array, objeto
+let meta = {
+    value: "Dormir mais cedo",
+    checked: false
+}
+
+let metas = [
+    meta,{
+        value: 'Faxer exercicios',
+        checked: false
+    }
+]
+
+console.log(metas.value)
